aefa8db06f2cb3850f1e64c50879937231ced3df
Lucidastar <lucidahxx@gmail.com>
2017-12-28 16:28:25 +0800

    更新

--- app/src/main/java/com/lucida/tsphoto/MainActivity.java
+++ app/src/main/java/com/lucida/tsphoto/MainActivity.java
@@ -10,6 +10,7 @@ import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -93,5 +94,23 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (imageUri != null) {
+            outState.putString("uriPath", imageUri.toString());
+        }
+//        Log.d(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (TextUtils.isEmpty(imageUri.toString())) {
+            imageUri = Uri.parse(savedInstanceState.getString("uriPath"));
+        }
+//        Log.d(TAG, "onRestoreInstanceState");
     }
 }
